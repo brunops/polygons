34d24d1bbaf8e22a4fe0ffcbe85294ce39ce967f
Bruno Sanches <brunopsanches@gmail.com>
2013-09-25 22:50:41 -0700

    Add Vertex Backbone Model and Vertices Backbone Collection

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-// hey
+require(['model', 'Vertex'], function(model, Vertex) {
+  model.on('add', function() {
+    console.log('aaaaadd!');
+  });
+  model.add(new Vertex());
+
+  console.log(model);
+});
---
+++ public/js/model.js
@@ -0,0 +1,7 @@
+define(['Vertex'], function(Vertex) {
+  var Vertices = Backbone.Collection.extend({
+    model: Vertex
+  });
+
+  return new Vertices();
+})
