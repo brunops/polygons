9f25a21274ca68b58d91d56c4ce55713dee6cb48
Bruno Sanches <brunopsanches@gmail.com>
2013-09-27 09:40:37 -0700

    Fix circle placements according to mouse click

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,14 @@
 require(['model', 'view', 'Vertex'], function(model, view, Vertex) {
 
+  var canvasPos  = $(canvas).position(),
+      canvasTop  = canvasPos.top,
+      canvasLeft = canvasPos.left;
+
 
   canvas.addEventListener('click', function(e) {
     var vertex = new Vertex();
-    vertex.set('x', e.pageX);
-    vertex.set('y', e.pageY);
+    vertex.set('x', e.pageX - canvasLeft);
+    vertex.set('y', e.pageY - canvasTop);
     model.add(vertex);
   });
 
