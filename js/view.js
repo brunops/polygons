c0001f688974aeb17a05d524bcb3f27b99f4a115
Bruno Sanches <brunopsanches@gmail.com>
2014-02-12 00:04:31 -0800

    Fill polygon formed by all vertices with a beautiful slateblue color

--- js/view.js
+++ js/view.js
@@ -5,14 +5,16 @@ define(['model'], function(model) {
     c.clearRect(0, 0, canvas.width, canvas.height);
 
     var prev = model.last();
+    c.beginPath();
+    c.moveTo(prev.get('x'), prev.get('y'));
     model.each(function(curr) {
-      c.beginPath();
-      c.moveTo(prev.get('x'), prev.get('y'));
       c.lineTo(curr.get('x'), curr.get('y'));
-      c.closePath();
-      c.stroke();
       prev = curr;
     });
+    c.closePath();
+    c.fillStyle = 'slateblue';
+    c.fill();
+    c.stroke();
 
     model.each(renderVertex);
   }
@@ -20,6 +22,7 @@ define(['model'], function(model) {
   function renderVertex(vertex) {
     c.beginPath();
     c.arc(vertex.get('x'), vertex.get('y'), vertex.get('radius'), 0, 2 * Math.PI);
+    c.fillStyle = '#000';
     c.fill();
   }
 
